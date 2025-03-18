dd03c6b329efc35a3729296ce88dd8b5bde81bbf
Professor SESI/SENAI <Professor@SN-371154.edu.fiesc.com.br>
2025-03-18 15:19:53 -0300

    novo

--- jogo.js
+++ jogo.js
@@ -1,7 +1,7 @@
 let button, quebraLinha, jogada = 1, vencedor = 0
-let tabuleiro = new Array(3)
+let tabuleiro = new Array(8)
 for(let i = 0; i < tabuleiro.length; i++){
-    tabuleiro[i] = new Array(3);
+    tabuleiro[i] = new Array(8);
 }
 for(let i = 0; i < tabuleiro.length; i++){
     quebraLinha = document.createElement('br')
@@ -12,6 +12,12 @@ for(let i = 0; i < tabuleiro.length; i++){
         button.setAttribute('id', 'bt' + i + "" + j)
         button.setAttribute('class', 'btJogo' + i)
         button.setAttribute('onclick', "marca(" + i + "," + j + ")")
+        if(i % 2 == 1 && j % 2 == 0){
+            button.style.backgroundColor = "black"
+        }
+        if(i % 2 == 0 && j % 2 == 1){
+            button.style.backgroundColor = "black"
+        }        
         button.append(document.createTextNode(""))
         document.body.append(button)
     }
